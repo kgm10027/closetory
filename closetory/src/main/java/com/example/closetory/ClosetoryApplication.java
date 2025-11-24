6becb88d20cd77ad80c19354058ec558105ac5ad
kgm10027 <kgm10027@gmail.com>
2025-11-24 13:10:17 +0900

    Add files via upload

---
+++ closetory/src/main/java/com/example/closetory/ClosetoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.closetory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetoryApplication.class, args);
+	}
+
+}
---
+++ closetory/src/test/java/com/example/closetory/ClosetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.closetory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
